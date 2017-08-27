5d8c9a7fb8e8c7565bf753c5a08e5072a6c5ec2c
hank205 <hank205@126.com>
2017-08-27 02:00:00 -0500

    update data
    update data

--- data.js
+++ data.js
@@ -1,23 +1,22 @@
 // Define your data array here
 var data = [
-{"tenant":"🍤🍤🍤","startDate":"08/04/2017","endDate":"09/01/2017"},
-{"tenant":"李","startDate":"08/04/2017","endDate":"09/02/2017"},
-{"tenant":"子豪","startDate":"08/04/2017","endDate":"09/03/2017"},
-{"tenant":"效天","startDate":"08/04/2017","endDate":"09/04/2017"},
+{"tenant":"🍤🍤🍤","startDate":"08/16/2017","endDate":"11/16/2017"},
+{"tenant":"李","startDate":"08/10/2017","endDate":"01/10/2018"},
+{"tenant":"效天","startDate":"08/21/2017","endDate":"12/21/2017"},
+{"tenant":"子豪","startDate":"08/25/2017","endDate":"09/03/2017"},
 
 {"tenant":"Hank","startDate":"08/04/2017","endDate":"08/04/2018"},
+{"tenant":"Hank2","startDate":"08/04/2018","endDate":"08/04/2018"},
+{"tenant":"Hank3","startDate":"08/04/2018","endDate":"08/04/2018"},
+{"tenant":"Hank4","startDate":"08/04/2018","endDate":"08/04/2018"},
 
-{"tenant":"🍤🍤🍤","startDate":"08/04/2018","endDate":"08/04/2018"},
-{"tenant":"李","startDate":"08/04/2018","endDate":"08/04/2018"},
-{"tenant":"子豪","startDate":"08/04/2018","endDate":"08/04/2018"},
-{"tenant":"效天","startDate":"08/04/2018","endDate":"08/04/2018"}
 ];
 
 tenants=[
 '🍤🍤🍤',
 '李',
-'子豪',
 '效天',
+'子豪',
 'Hank',
 'Hank2',
 'Hank3',
