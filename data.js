1c54ebd3fcb168739ac2d77b3a848932a80b4965
hank205 <hank205@126.com>
2017-11-22 23:17:12 -0600

    update data

--- data.js
+++ data.js
@@ -3,12 +3,12 @@ var data = [
 {"tenant":"🍤🍤🍤","startDate":"08/16/2017","endDate":"11/16/2017"},
 {"tenant":"李","startDate":"08/10/2017","endDate":"01/10/2018"},
 {"tenant":"效天","startDate":"08/21/2017","endDate":"12/21/2017"},
-{"tenant":"子豪","startDate":"08/25/2017","endDate":"09/03/2017"},
+{"tenant":"子豪","startDate":"08/25/2017","endDate":"12/25/2017"},
 
-{"tenant":"Hank","startDate":"08/04/2017","endDate":"08/04/2018"},
-{"tenant":"Hank2","startDate":"08/04/2018","endDate":"08/04/2018"},
-{"tenant":"Hank3","startDate":"08/04/2018","endDate":"08/04/2018"},
-{"tenant":"Hank4","startDate":"08/04/2018","endDate":"08/04/2018"},
+{"tenant":"Sezz","startDate":"12/22/2017","endDate":"1/13/2018"},
+{"tenant":"Airbnb_Edison","startDate":"11/23/2017","endDate":"11/25/2017"},
+{"tenant":"Airbnb_Muhammad","startDate":"11/27/2017","endDate":"11/27/2017"},
+{"tenant":"Airbnb_Taco","startDate":"12/01/2017","endDate":"12/21/2017"},
 
 ];
 
@@ -17,8 +17,8 @@ tenants=[
 '李',
 '效天',
 '子豪',
-'Hank',
-'Hank2',
-'Hank3',
-'Hank4'
+'Sezz',
+'Airbnb_Edison',
+'Airbnb_Muhammad',
+'Airbnb_Taco'
 ];
